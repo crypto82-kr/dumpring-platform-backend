c3d6b2d8723ea765b61bbe9129fd486e2cd3905b
leeteawoo <stunnering@naver.com>
2026-05-27 19:13:35 +0900

    config(web): allow local IP and localtunnel in dev HMR allowedDevOrigins

--- dumpring_web/next.config.ts
+++ dumpring_web/next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: [
+    "192.168.0.7",
+    "192.168.0.7:3000",
+    "localhost:3000",
+    "*.loca.lt",
+    "*.locallt.me"
+  ]
 };
 
 export default nextConfig;
